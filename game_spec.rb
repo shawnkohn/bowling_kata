ff47c71925bf3ddbb1b9c7e23f4dc7e4f8cf0205
Shawn Kohn <shawnkohn@gmail.com>
2012-02-01 10:45:27 -0500

    first test passing

--- game_spec.rb
+++ game_spec.rb
@@ -1,9 +1,13 @@
 # game_spec.rb
 
+require './game'
+
 describe "Game" do
 	describe "#Score" do
 		it "Should be 0 when rolling all gutter balls" do
-
+			game = Game.new
+			20.times { game.roll(0) }
+			game.score.should == 0
 		end
 	end
 end
